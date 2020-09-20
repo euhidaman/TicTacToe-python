e8cae731896a6a74d4b56cd9e72a596c3429a85f
Euhid Aman <aman.derax20@gmail.com>
2020-09-20 16:53:41 +0530

    wrote the display_board() function, to display game board

--- tictactoe.py
+++ tictactoe.py
@@ -9,3 +9,14 @@
   # -check diagonal
 # check tie
 # flip player
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+display_board()
