899ad1dc6fef227d8adf5b1ed363c22bc3c5b679
Euhid Aman <aman.derax20@gmail.com>
2020-09-21 22:40:00 +0530

    declared game global variables

--- tictactoe.py
+++ tictactoe.py
@@ -10,9 +10,20 @@
 # check tie
 # flip player
 
+
+#------------GlobalVariable-------------
 board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
+
+# if Game still goin on
+game_still_going = True
+
+# Who won ? or Tie?
+winner = None
+
+# Who's turn , is it?
+current_player = "X"
 
 def display_board():
     print(board[0] + " | " + board[1] + " | " + board[2])
@@ -24,13 +35,13 @@ def play_game():
     display_board()
 
     while game_still_going:
-        handle_turn(current _player)
+        handle_turn(current_player)
 
         check_if_game_over()
 
         flip_player()
 
-def handle_turn():
+def handle_turn(player):
     position = input("Choose a position from 1 to 9: ")
     position = int(position)-1
 
@@ -40,5 +51,17 @@ def handle_turn():
 def check_if_game_over():
     check_if_win()
     check_if_tie()
+
+def check_if_win():
+    # check rows
+    # check columns
+    # check diagonals
+    return
+
+def check_if_tie():
+    return
+
+def flip_player():
+    return
 
 play_game()
