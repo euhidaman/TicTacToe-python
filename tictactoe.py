ad4e8507b1a9a6149e45fb02a1e6e378f571d671
Euhid Aman <aman.derax20@gmail.com>
2020-09-21 22:24:32 +0530

    wrote skeleton function to handle_turn() and check_if_game_over().

--- tictactoe.py
+++ tictactoe.py
@@ -19,4 +19,26 @@ def display_board():
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-display_board()
+def play_game():
+    # display initial board
+    display_board()
+
+    while game_still_going:
+        handle_turn(current _player)
+
+        check_if_game_over()
+
+        flip_player()
+
+def handle_turn():
+    position = input("Choose a position from 1 to 9: ")
+    position = int(position)-1
+
+    board[position] = "X"
+    display_board()
+
+def check_if_game_over():
+    check_if_win()
+    check_if_tie()
+
+play_game()
