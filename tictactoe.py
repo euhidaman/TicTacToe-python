6321dc484c34d205a45cf00e6d1fd7790207eef2
Euhid Aman <aman.derax20@gmail.com>
2020-09-23 19:49:38 +0530

    wrote skeleton functions to check winner in rows,columns and diagonals

--- tictactoe.py
+++ tictactoe.py
@@ -25,22 +25,36 @@ winner = None
 # Who's turn , is it?
 current_player = "X"
 
+# display board
 def display_board():
     print(board[0] + " | " + board[1] + " | " + board[2])
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
+# play game tic tac toe
 def play_game():
     # display initial board
     display_board()
 
+    # while the game is still going on
     while game_still_going:
+
+        # handle a single turn of an arbitraray player
         handle_turn(current_player)
 
+        # check if the game has ended
         check_if_game_over()
 
+        # flip to the other player
         flip_player()
 
+    # the game has ended
+    if winner =='X' or winner =='O':
+        print(winner + ' won.')
+    elif winner == None :
+        print("Tie.")
+
+# handle a single turn of an arbitraray player
 def handle_turn(player):
     position = input("Choose a position from 1 to 9: ")
     position = int(position)-1
@@ -49,13 +63,40 @@ def handle_turn(player):
     display_board()
 
 def check_if_game_over():
-    check_if_win()
+    check_for_winner()
     check_if_tie()
 
-def check_if_win():
+def check_for_winner():
+    # set up global variable
+    global winner
     # check rows
+    row_winner = check_rows()
+
     # check columns
+    column_winner = check_columns()
+
     # check diagonals
+    diagonal_winner = check_diagonals()
+
+    if row_winner:
+        winner  = row_winner()
+    elif column_winner:
+        winner = column_winner()
+    elif diagonal_winner:
+        winner = diagonal_winner()
+    else:
+        # there was no win
+        winner = None
+
+    return
+
+def check_rows():
+    return
+
+def check_columns():
+    return
+
+def check_diagonals():
     return
 
 def check_if_tie():
